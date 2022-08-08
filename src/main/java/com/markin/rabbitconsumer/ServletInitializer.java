776bbca90423310489cc2b9bca250decc7e07c62
Jonathan Ato Markin <jona10711@gmail.com>
2022-08-08 13:24:30 +0000

    Rabbit consumer

---
+++ src/main/java/com/markin/rabbitconsumer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.markin.rabbitconsumer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RabbitConsumerApplication.class);
+    }
+
+}
